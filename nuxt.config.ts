4972a481aa5f3bcd2ab074bd163e7064b7c34d00
Steven Zhong <89566389+y7516552@users.noreply.github.com>
2024-11-22 18:01:02 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ["@/assets/stylesheets/all.scss"],
-  // modules: ["nuxt-swiper"],
   "components": {
    "dirs": [
      {
